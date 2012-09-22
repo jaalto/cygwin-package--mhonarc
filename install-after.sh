8b037ed713738a94afdef1388ee3d6c78b605e6f
Jari Aalto <jari.aalto@cante.net>
2012-09-22 15:32:40 +0300

    import original  CYGWIN-PATCHES 2012-09-22

---
+++ install-after.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# Copyright (C) 2008 Jari Aalto; Licensed under GPL v2 or later
+#
+# install-after.sh -- Custom installation
+#
+# The script will receive one argument: relative path to
+# installation root directory. Script is called like:
+#
+#    $ install-after.sh .inst
+#
+# This script is run after [install] command. NOTE: Echo all messages
+# with ">> " prefix".
+
+PATH="/sbin:/usr/sbin/:/bin:/usr/bin:/usr/X11R6/bin"
+LC_ALL="C"
+
+set -e
+
+Cmd()
+{
+    echo "$@"
+    [ "$test" ] && return
+    "$@"
+}
+
+Main()
+{
+    root=${1:-".inst"}
+
+    if [ ! "$root"  ] || [ ! -d "$root" ]; then
+        echo "$0: [ERROR] In $(pwd) no such directory: '$root'" >&2
+        return 1
+    fi
+
+    root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
+    sharedir=$root/usr/share
+
+    docpkg=$(cd $root/usr/doc && pwd)
+
+    [ "$docpkg" ] || return 0
+
+    echo ">> Removing extra doc/ (already handled)"
+    Cmd rm -rf $docpkg
+}
+
+Main "$@"
+
+# End of file
